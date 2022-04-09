1f9cfd1de2c9e4a53c94da7d8303d6f5f009e6ff
Mark Markaryan <markmark206@gmail.com>
2022-04-09 03:29:39 -0700

    setup.py: cleanup

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
-# from setuptools import find_packages, setup
 from distutils.core import setup
 
 setup(
-    name='log-indented',
-    # packages=find_packages(),
-    version='0.1.0',
-    description='A logging utility that makes your logs easy to grok (stack-depth-indented!) and use (deocrator!)',
-    author='Merk Markaryan',
-    author_email='markmark205+log_indented@gmail.com',
-    url='https://github.com/markmark206/log-indented',
-    # py_modules=['log-indented'],
+    name="log_indented",
+    version="0.1.0",
+    description="A logging utility that makes your logs easy to grok (stack-depth-indented!) and use (deocrator!)",
+    author="Merk Markaryan",
+    author_email="markmark206+log_indented@gmail.com",
+    url="https://github.com/markmark206/log-indented",
     packages=["log_indented"],
-    # package_dir={'': 'src'},
-    license='MIT',
+    license="MIT",
 )
