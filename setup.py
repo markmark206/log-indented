463dfad0438df37902e87f0e02a327ffa93167b6
Mark Markaryan <markmark206@gmail.com>
2022-04-09 21:34:38 -0700

    adding setup.cfg

--- setup.py
+++ setup.py
@@ -1,12 +1,2 @@
-from distutils.core import setup
-
-setup(
-    name="log_indented",
-    version="0.1.0",
-    description="A logging utility that makes your logs easy to grok (stack-depth-indented!) and use (deocrator!)",
-    author="Merk Markaryan",
-    author_email="markmark206+log_indented@gmail.com",
-    url="https://github.com/markmark206/log-indented",
-    packages=["log_indented"],
-    license="MIT",
-)
+from setuptools import setup
+setup(use_scm_version=True)
